9abcc01e365f45307ae7ad57356a14a3b9467bcc
Aitor I Bertorelli E <aitor.imanol@gmail.com>
2014-11-27 21:22:09 +0100

    Cambios de scripts
    Incluir embed para que cargue

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,18 @@
-$(function(){
-	
-	var timeline = new VMM.Timeline();
-	timeline.init("data.json");
+//$(function(){
+//	
+//	var timeline = new VMM.Timeline();
+//	timeline.init("data.json");
+//
+//});
 
+
+$(document).ready(function() {
+    createStoryJS({
+        type:       'timeline',
+        width:      '',
+        height:     '',
+        source:     'https://docs.google.com/spreadsheet/pub?key=0Am0OdXdnisZAdGJ5QXVoeXVLcEV2akFvQTNaTGM5S1E&output=html',
+        embed_id:   'my-timeline',
+        debug:		true
+    });
 });
