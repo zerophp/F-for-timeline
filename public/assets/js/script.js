b24457e718f100f23e83709f6d4b758bbe1e6b7e
Aitor I Bertorelli E <aitor.imanol@gmail.com>
2014-12-01 00:08:16 +0100

    Timeline reparado
    - Ejemplo no funciona con spreadsheet por cambio en Google API.
    - timeline.js 2014-11-26 no posee mismo html que ejemplo y CSS no
    concuerda.
    - Tweak del theme timeline.js 2014-11-26 para usar theme dark hasta
    conseguir método js nativo.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,18 +1,14 @@
-//$(function(){
-//	
-//	var timeline = new VMM.Timeline();
-//	timeline.init("data.json");
-//
-//});
 
+$(function(){
+	
+	var timeline = new VMM.Timeline();
+	timeline.init('spreadsheet:0AjIy08LtjWt4dEMwRlVBVzdudmlBRW5qcjJ4aGd3S2c&output=html');
+//	timeline.init("assets/data.json");
 
-$(document).ready(function() {
-    createStoryJS({
-        type:       'timeline',
-        width:      '',
-        height:     '',
-        source:     'https://docs.google.com/spreadsheet/pub?key=0Am0OdXdnisZAdGJ5QXVoeXVLcEV2akFvQTNaTGM5S1E&output=html',
-        embed_id:   'my-timeline',
-        debug:		true
-    });
 });
+//$(document).ready(function(){
+//	 var timeline_config = {
+//            width:              '100%',
+//            height:             '600'
+//		};
+//});
