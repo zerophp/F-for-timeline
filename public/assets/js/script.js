b854bcd41a5c599c76ad5631f4e63784db8ea52b
Aitor I Bertorelli E <aitor.imanol@gmail.com>
2014-12-01 00:31:33 +0100

    Comentarios para intercambiar opt/js por js
    - Se manipuló index.html de tal forma de poder intercambiar líneas
    comentadas y usar assets originales u opt.
    - Devuelto js/script.js a estado original

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,13 +2,6 @@
 $(function(){
 	
 	var timeline = new VMM.Timeline();
-	timeline.init('spreadsheet:0AjIy08LtjWt4dEMwRlVBVzdudmlBRW5qcjJ4aGd3S2c&output=html');
-//	timeline.init("assets/data.json");
+	timeline.init("assets/data.json");
 
 });
-//$(document).ready(function(){
-//	 var timeline_config = {
-//            width:              '100%',
-//            height:             '600'
-//		};
-//});
